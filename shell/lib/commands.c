74dadc7fe1bab4b32c6a13bad876714d90bb95ee
khalil hadji <h-adji_tech@proton.me>
2026-01-19 17:58:56 +0100

    implement builtin exit

--- shell/lib/commands.c
+++ shell/lib/commands.c
@@ -6,25 +6,45 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-char *builtin_commands[] = {"cd", "exit"};
+int builtin_cd(char **args) { return 0; }
+int builtin_exit(char **args) {
+  // Implement any cleanup here before exiting
+  printf("Exiting shell...\n");
+  exit(EXIT_SUCCESS);
+  // Unreachable, but good practice to show return type
+  return 0;
+}
+typedef int (*builtin_cmd_ptr)(char **args);
 
-int is_builtin_cmd(char *cmd) {
-  int n = sizeof(builtin_commands) / sizeof(builtin_commands[0]);
-  for (int i = 0; i < n; i++) {
-    if (strcmp(cmd, builtin_commands[i]) == 0) {
-      return 1;
+typedef struct {
+  char *name;
+  builtin_cmd_ptr func;
+} builtin_cmd;
+
+builtin_cmd builtin_commands[] = {
+    {"exit", builtin_exit},
+};
+int builtin_cmd_count = sizeof(builtin_commands) / sizeof(builtin_commands[0]);
+
+int is_builtin_cmd(char **args) {
+  char *cmd = args[0];
+  for (int i = 0; i < builtin_cmd_count; i++) {
+    if (strcmp(cmd, builtin_commands[i].name) == 0) {
+      return builtin_commands[i].func(args);
     }
   }
-  return 0;
+  return EXIT_FAILURE;
 }
 
 void execute_cmd(char *args[]) {
-  pid_t pid = fork();
-  if (pid == 0) {
-    if (execvp(args[0], args) < 0) {
-      perror(args[0]);
-      exit(EXIT_FAILURE);
+  if (is_builtin_cmd(args) == EXIT_FAILURE) {
+    pid_t pid = fork();
+    if (pid == 0) {
+      if (execvp(args[0], args) < 0) {
+        perror(args[0]);
+        exit(EXIT_FAILURE);
+      }
     }
+    waitpid(pid, NULL, 0);
   }
-  waitpid(pid, NULL, 0);
 }
